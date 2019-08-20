33b3ef1b69a3fa1b1266d4f0da11ba84cd34ec6c
powerteks <jit.prophet@gmail.com>
2019-08-20 08:51:47 +0300

    finished footer

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,6 @@
 
 
 function checkFooterForm() {
-	var footerEmail = document.querySelector('footer__form-input');
-	document.querySelector('footer__form-button').disabled = footerEmail.value ? false : "disabled";
+	var footerEmail = document.querySelector('.footer__form-input');
+	document.querySelector('.footer__form-button').disabled = footerEmail.value ? false : "disabled";
 }
